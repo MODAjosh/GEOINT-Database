106e9f20cb944eeac8a02967bc867f4d5f319c55
MODA - Josh <Josh@Cpsemo.org>
2025-01-02 10:06:37 -0600

    Update shapefile_to_geojson.py

--- code/data_preprocessing/shapefile_to_geojson.py
+++ code/data_preprocessing/shapefile_to_geojson.py
@@ -1,1 +1,14 @@
+import geopandas as gpd
+
+# Input shapefile
+shapefile = 'shapefiles/your_shapefile.shp'
+output_geojson = 'processed_data/your_data.geojson'
+
+# Load the shapefile using GeoPandas
+gdf = gpd.read_file(shapefile)
+
+# Convert to GeoJSON and save
+gdf.to_file(output_geojson, driver='GeoJSON')
+
+print(f"Shapefile {shapefile} has been converted to GeoJSON and saved to {output_geojson}.")
 
